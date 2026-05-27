467fdb121e6494c73ffbbb8da87b490fbe435b11
Jenil Jetpuriya <jeniljetpuriya05@gmail.com>
2026-05-27 14:34:48 +0530

    Update server run configuration to use environment port

--- app.py
+++ app.py
@@ -855,4 +855,7 @@ if __name__ == '__main__':
     print("Navigate to: http://127.0.0.1:5000")
     print("=" * 70)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
