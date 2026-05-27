40d0df081b9031d5b915f24f12eb583e4680fbb9
Jenil Jetpuriya <jeniljetpuriya05@gmail.com>
2026-05-27 14:42:15 +0530

    Fix indentation for main block in app.py

--- app.py
+++ app.py
@@ -855,7 +855,7 @@ if __name__ == '__main__':
     print("Navigate to: http://127.0.0.1:5000")
     print("=" * 70)
     
-    if __name__ == '__main__':
+if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
